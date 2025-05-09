9516232ecc38ad0353787284fb7d73bffa39aff4
Nicolas Martin <nicolas.martin3@radiofrance.com>
2025-05-09 22:03:32 +0200

    clean old back-end logic

--- src/app/components/navigation/nav.tsx
+++ src/app/components/navigation/nav.tsx
@@ -7,7 +7,6 @@ const Nav = () => {
       <ul className="mx-auto w-fit flex gap-4 uppercase">
         <li>
           <Link href="/">Home</Link>
-          <Link href="/formule">Saison 2024</Link>
           <Link href="/session">Saison 2025</Link>
         </li>
       </ul>
